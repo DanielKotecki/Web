a3629f366d937d58b2b038d8650138fdad2f2e3e
DanielKotecki <48563941+DanielKotecki@users.noreply.github.com>
2020-01-25 21:31:04 +0100

    Add files via upload

---
+++ Baza Danych/testowa.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Czas generowania: 11 Sty 2020, 13:14
+-- Wersja serwera: 10.1.38-MariaDB
+-- Wersja PHP: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `testowa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tab`
+--
+
+CREATE TABLE `tab` (
+  `id` int(11) NOT NULL,
+  `marka` varchar(20) COLLATE utf8_polish_ci NOT NULL,
+  `model` varchar(25) COLLATE utf8_polish_ci NOT NULL,
+  `moc_silnika` int(11) NOT NULL,
+  `dos` varchar(50) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `tab`
+--
+
+INSERT INTO `tab` (`id`, `marka`, `model`, `moc_silnika`, `dos`) VALUES
+(4, 'Alfa Romeo', '159', 290, 'dostepne'),
+(7, 'Audi', 'A6', 345, 'niedostepne');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `tab`
+--
+ALTER TABLE `tab`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `tab`
+--
+ALTER TABLE `tab`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
